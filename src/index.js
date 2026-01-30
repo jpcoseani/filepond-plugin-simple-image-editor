b2dd8b179d2e9ddc2c02e204dc3696065b47185c
jpcoseani <jpcoseani@gmail.com>
2026-01-30 15:57:38 -0300

    Use updated flip icons

--- src/index.js
+++ src/index.js
@@ -13,14 +13,14 @@ const actionIcons = {
     <path d="M21.01,12A8.994,8.994,0,0,1,5.64,18.36l2.374-2.374H1.993a.956.956,0,0,0-.955.955v6.021l2.491-2.491A11.98,11.98,0,0,0,24,12Z" fill="${iconStroke}" />
   </svg>`,
   'flip-horizontal': `<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-    <path d="M12 5V7M12 11V13M12 17V19" stroke="${iconStroke}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    <path d="M20 7L15 12L20 17V7Z" stroke="${iconStroke}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    <path d="M4 7L9 12L4 17V7Z" stroke="${iconStroke}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"></rect>
+    <path d="M6.09 19h12l-1.3 1.29a1 1 0 0 0 1.42 1.42l3-3a1 1 0 0 0 0-1.42l-3-3a1 1 0 0 0-1.42 0 1 1 0 0 0 0 1.42l1.3 1.29h-12a1.56 1.56 0 0 1-1.59-1.53V13a1 1 0 0 0-2 0v2.47A3.56 3.56 0 0 0 6.09 19z" fill="${iconStroke}" />
+    <path d="M5.79 9.71a1 1 0 1 0 1.42-1.42L5.91 7h12a1.56 1.56 0 0 1 1.59 1.53V11a1 1 0 0 0 2 0V8.53A3.56 3.56 0 0 0 17.91 5h-12l1.3-1.29a1 1 0 0 0 0-1.42 1 1 0 0 0-1.42 0l-3 3a1 1 0 0 0 0 1.42z" fill="${iconStroke}" />
   </svg>`,
   'flip-vertical': `<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-    <path d="M12 5V7M12 11V13M12 17V19" stroke="${iconStroke}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    <path d="M20 7L15 12L20 17V7Z" stroke="${iconStroke}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    <path d="M4 7L9 12L4 17V7Z" stroke="${iconStroke}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0"></rect>
+    <path d="M5 6.09v12l-1.29-1.3a1 1 0 0 0-1.42 1.42l3 3a1 1 0 0 0 1.42 0l3-3a1 1 0 0 0 0-1.42 1 1 0 0 0-1.42 0L7 18.09v-12A1.56 1.56 0 0 1 8.53 4.5H11a1 1 0 0 0 0-2H8.53A3.56 3.56 0 0 0 5 6.09z" fill="${iconStroke}" />
+    <path d="M14.29 5.79a1 1 0 0 0 1.42 1.42L17 5.91v12a1.56 1.56 0 0 1-1.53 1.59H13a1 1 0 0 0 0 2h2.47A3.56 3.56 0 0 0 19 17.91v-12l1.29 1.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-3-3a1 1 0 0 0-1.42 0z" fill="${iconStroke}" />
   </svg>`,
 };
 
